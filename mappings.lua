9cce25531f90dd8c23bdc90dec87d225355fc6f9
Leo-Clerc <leocoursadistance@gmail.com>
2023-07-10 08:05:20 +0200

    Add some mappings

--- mappings.lua
+++ mappings.lua
@@ -20,9 +20,37 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- Those ones are here to be used on azerty keyboard
+    ["à"] = {"@", desc = "Macro"},
+    ["µ"] = {"#", desc = "Search previous word under cursor"},
+    ["è"] = {"`", desc = "Goes to exact spot of the mark"},
+    ["é"] = {"", desc = ""},
+    ["ç"] = {"{", desc = "Move one paragraph backward"},
+    ["<C-ç>"] = {"}", desc = "Move one paragrah forward"},
+    ["^"] = {"^<leader>", desc = "Tired of having to press space everytime afterward"},
+    ["§"] = {"\\", desc = "Split window in two horizonatally ?"},
+    ["<A-("] = {"[", desc = "Bracket"},
+    ["<A-)"] = {"]", desc = "Bracket"},
+
+    -- Useful mappings
+    ["<C-u>"] = {"<C-u>zz", desc = "Move up half a screen"}
+    ["<C-d>"] = {"<C-d>zz", desc = "Move down half a screen"}
+    ["<C-b>"] = {":bprevious<CR>", desc = "Previous buffer"}
+    ["<C-n>"] = {":bnext<CR>", desc = "Next buffer"}
+
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  i = {
+    ["<A-("] = {"[", desc = "Bracket"}
+    ["<A-)"] = {"]", desc = "Bracket"}
+    ["§"] = {"\\", desc = "Who the heck uses § anyway ???"}
+  },
+  v = {
+    ["p"] = {"_dP", desc = "Replace selection with content in buffer"}
+  }
 }
+
+
